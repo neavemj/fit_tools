c056e99ce21a28d18d14d496b87478da153b687d
neavemj <matthewjneave1@gmail.com>
2020-01-10 17:08:39 +1100

    minor edits

--- parse_fit.py
+++ parse_fit.py
@@ -7,8 +7,22 @@ and extract useful information
 
 from fitparse import FitFile
 import sys
+import time
 
-fit = FitFile(sys.argv[1])
+#fit_fl = FitFile(sys.argv[1])
+fit_fls = sys.argv[1:]
 
-for record in fit:
-    print(record)
+for fls in fit_fls:
+    fit_fl = FitFile(fls)
+
+    for record in fit_fl.get_messages("sport"):
+        print("new_record")
+        values = record.get_values()
+        print(values)
+        print("\n")
+        break
+
+
+
+#ts = values["local_timestamp"]
+#print(ts.strftime("%Y-%m-%d %H:%M:%S"))
